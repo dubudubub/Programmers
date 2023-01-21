6b2078da312f3cda04fdb1145b8de4d06a323e91
eunji <113995690+dubudubub@users.noreply.github.com>
2023-01-21 18:51:21 +0900

    [level 2] Title: 입양 시각 구하기(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
+++ 프로그래머스/lv2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
@@ -1,6 +1,7 @@
 -- 코드를 입력하세요
 select to_char(datetime,'hh24')as hour, count(to_char(datetime,'hh'))as count from animal_outs
-where to_char(datetime,'hh24') > 08
-and to_char(datetime,'hh24') < 20
+--where to_char(datetime,'hh24') > 08
+--and to_char(datetime,'hh24') < 20
+where to_char(datetime,'hh24') between 09 and 20
 group by to_char(datetime,'hh24')
 order by hour
